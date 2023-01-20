3d6ffbea56da38b9a58861fe2f41c8ae719050b2
Ryan Will Daros <ryan_will-@hotmail.com>
2023-01-20 15:28:07 -0300

    Iniciado SummaryTable

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -33,6 +33,49 @@ await prisma.habit.create({
   }
 })
 });
+
+
+app.get('/day', async(request) => {
+  const getDayParams = z.object({
+    date: z.coerce.date()
+  })
+
+  const {date} = getDayParams.parse(request.query)
+  const parsedDate = dayjs(date).startOf('day')
+  const weekDay = parsedDate.get('day')
+
+  const possibleHabits = await prisma.habit.findMany({
+    where: {
+      created_at: {
+        lte: date,
+      },
+      weekDays: {
+        some: {
+          week_day: weekDay,
+        }
+      }
+    }
+  })
+
+
+const day = await prisma.day.findUnique({
+  where:{
+    date: parsedDate.toDate(),
+  },
+  include: {
+    dayHabits: true,
+  }
+})
+const completeHabits = day?.dayHabits.map(dayHabits => {
+  return dayHabits.habit_id
+})
+
+
+  return{
+    possibleHabits,
+    
+  }
+})
 }
 
 
